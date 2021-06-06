c2022d3d7e8083e57091ad57eb8bbf25c4c2fea5
xiecoder <3318461331@qq.com>
2021-06-06 22:31:28 +0800

    feat: add login interface

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,25 +1,87 @@
 <template>
   <div class="login">
-    <el-button type="primary">主要按钮</el-button>
+    <div class="box">
+      <div class="header">
+        <span class="title">
+          植物病虫害管理系统
+        </span>
+      </div>
+      <div class="content">
+        <el-form ref="loginFormRef" :model="loginForm">
+          <el-form-item prop="username" :rules="rules.username">
+            <el-input
+              placeholder="账号"
+              prefix-icon="el-icon-user-solid"
+              v-model="loginForm.username"
+            />
+          </el-form-item>
+          <el-form-item prop="password" :rules="rules.password">
+            <el-input
+              type="password"
+              placeholder="密码"
+              :show-password="true"
+              prefix-icon="el-icon-lock"
+              v-model="loginForm.password"
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button class="button" type="primary" @click="submitForm">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, toRefs } from 'vue';
+import {
+  ref,
+  toRefs,
+  reactive,
+  defineComponent
+} from 'vue';
+
+interface userForm {
+  username: string;
+  password: string;
+}
 
 export default defineComponent({
   prop: {},
   components: {},
   setup () {
-    const loading = ref(false);
-    const loginForm = ref({
-      username: 'root',
-      password: 'root'
+    const state = reactive({
+      loginForm: {
+        username: 'root',
+        password: 'root'
+      } as userForm,
+      loginFormRef: ref(),
+      rules: {
+        username: [
+          { required: true, message: '请输入账号', trigger: 'change' },
+          { min: 4, max: 14, message: '账号长度为 4-16 位', trigger: 'change' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'change' },
+          { min: 4, max: 14, message: '密码长度为 4-16 位', trigger: 'change' }
+        ]
+      }
     });
-    const rules = ref({
-      username: [{ required: true, message: '请输入账号' }],
-      password: [{ required: true, message: '请输入密码' }]
-    });
+    const submitForm = () => {
+      state.loginFormRef.validate().then((valid: boolean) => {
+        console.log(valid);
+        if (valid) {
+          // 登陆
+          console.log(state.loginForm);
+        }
+      });
+    };
+    return {
+      ...toRefs(state),
+      submitForm
+    };
   }
 });
 </script>
@@ -30,9 +92,38 @@ export default defineComponent({
   height: 100vh;
   display: flex;
   overflow: hidden;
-  justify-content: center;
   flex-direction: column;
+  justify-content: center;
   background-color: rgb(240, 242, 245);
   background-image: url("~@/assets/images/login-background.svg");
+
+  .box {
+    top: -15vh;
+    width: 520px;
+    height: 300px;
+    margin: 0 auto;
+    position: relative;
+    text-align: center;
+
+    .header {
+      height: 120px;
+
+      .title {
+        color: black;
+        font-size: 30px;
+        font-weight: 700;
+      }
+    }
+
+    .content {
+      width: 360px;
+      height: 237px;
+      margin: 0 auto;
+
+      .button {
+        width: 360px;
+      }
+    }
+  }
 }
 </style>
