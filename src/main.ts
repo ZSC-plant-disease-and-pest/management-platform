3308e34c3f28bd0284447f7842d4ee5712b2eab2
xiecoder <3318461331@qq.com>
2021-06-06 22:29:36 +0800

    style: change ElementPlus to element

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import App from './App.vue';
 import axios from 'axios';
 import store from './store';
 import router from './router';
-import ElementPlus from 'element-plus';
+import element from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app');
+createApp(App).use(store).use(router).use(element).mount('#app');
