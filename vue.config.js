0ff165a93ece5bacf252c95be1075a8e048a946b
xiecoder <3318461331@qq.com>
2021-08-09 23:34:17 +0800

    fix: fix api target

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
 		port: 8081,
 		proxy: {
 			"/api": {
-				target: "https://localhost:8080/api/",
+				target: "http://localhost:8080/api/",
 				changeOrigin: true,
 				pathRewrite: {
 					'^/api': ''
