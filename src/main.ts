394b37b18667e5808365be15aa29cdbe8fd0073c
xiecoder <3318461331@qq.com>
2021-08-10 23:50:59 +0800

    feat: update to chinese display

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import store from './store';
 import router from './router';
 import element from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+import locale from 'element-plus/lib/locale/lang/zh-cn';
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 
-createApp(App).use(store).use(router).use(element, { zhCn }).mount('#app');
+createApp(App).use(store).use(router).use(element, { locale }).mount('#app');
