f0fd24ca72d13191a79949afdd5b088d6423d9b3
xiecoder <3318461331@qq.com>
2021-11-27 23:43:00 +0800

    feat: 新增echarts图形

--- src/views/admin/home/index.vue
+++ src/views/admin/home/index.vue
@@ -1,152 +1,76 @@
 <template>
-  <div style="display: flex; justify-content: space-between">
-    <el-card class="picture-tagging" style="width: 49%; height: 40%">
-      <template #header>
-        <div class="card-header">
-          <span> 待标注图片 </span>
-        </div>
-      </template>
-      <el-table :data="taggingTableData" style="width: 100%">
-        <el-table-column
-          v-for="item in taggingTableColumn"
-          :key="item.prop"
-          :prop="item.prop"
-          :label="item.label"
-          :width="item.width"
-        />
-        <el-table-column
-        label="操作"
-        width="119"
-        >
-          <template #default="scope">
-            <el-button
-              type="text"
-              size="small"
-              style="color: rgb(63, 186, 246)"
-              @click="check(scope.row)"
-            >
-              查看
-            </el-button>
-            <el-button
-              type="text"
-              size="small"
-              style="color: rgb(65, 209, 204)"
-              @click="edit(scope.row)"
-            >
-              编辑
-            </el-button>
-            <el-button
-              type="text"
-              size="small"
-              style="color: rgb(244, 96, 84)"
-              @click="remove(scope.row)"
-            >
-              删除
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-card>
-    <el-card class="picture-tagging" style="width: 49%; height: 40%">
-      <template #header>
-        <div class="card-header">
-          <span> 待标注图片 </span>
-        </div>
-      </template>
-      <el-table :data="taggingTableData" style="width: 100%">
-        <el-table-column
-          v-for="item in taggingTableColumn"
-          :key="item.prop"
-          :prop="item.prop"
-          :label="item.label"
-          :width="item.width"
-        />
-        <el-table-column
-        label="操作"
-        width="119"
-        >
-          <template #default="scope">
-            <el-button
-              type="text"
-              size="small"
-              style="color: rgb(63, 186, 246)"
-              @click="check(scope.row)"
-            >
-              查看
-            </el-button>
-            <el-button
-              type="text"
-              size="small"
-              style="color: rgb(65, 209, 204)"
-              @click="edit(scope.row)"
-            >
-              编辑
-            </el-button>
-            <el-button
-              type="text"
-              size="small"
-              style="color: rgb(244, 96, 84)"
-              @click="remove(scope.row)"
-            >
-              删除
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-card>
-  </div>
+  <el-row>
+    <el-col>
+      home
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col>
+      <div id="disease-echarts"></div>
+    </el-col>
+  </el-row>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  reactive
-} from 'vue';
+import { defineComponent, onMounted } from 'vue';
+import * as echarts from 'echarts';
 
 export default defineComponent({
   setup () {
-    const pictureTagging = reactive([
-      {
-        userId: '20210721001'
+    onMounted(() => {
+      let diseaseEcharts: any = '';
+      const diseaseDiv = document.getElementById('disease-echarts');
+
+      if (diseaseDiv) {
+        diseaseEcharts = echarts.init(diseaseDiv);
       }
-    ]);
-    const taggingTableData = reactive([
-      {
-        userId: '20210721001',
-        name: '月季'
-      },
-      {
-        userId: '20210721002',
-        name: '云杉'
-      },
-      {
-        userId: '20210721003',
-        name: '日本樱花'
-      },
-      {
-        userId: '20210721010',
-        name: '麒麟掌'
-      }
-    ]);
-    const taggingTableColumn = reactive([
-      {
-        prop: 'userId',
-        label: '用户编号',
-        width: 'auto'
-      },
-      {
-        prop: 'name',
-        label: '名称',
-        width: 'auto'
-      }
-    ]);
-    return {
-      pictureTagging,
-      taggingTableData,
-      taggingTableColumn
-    };
+      const option = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Direct',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+          }
+        ]
+      };
+      diseaseEcharts.setOption(option);
+    });
+
+    return {};
   }
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#disease-echarts {
+  height: 200px;
+  width: 200px;
+}
+</style>
