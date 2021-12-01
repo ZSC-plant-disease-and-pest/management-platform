29fa48307eb59360ec08f66727d1b8befdc86f5d
13113232864 <3318461331@qq.com>
2021-12-01 11:10:55 +0800

    refactor: 重构封装上传图片组件

---
+++ src/components/common/BasicImageUpload/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-upload
+    ref="uploadImageRef"
+    action="#"
+    :limit="10"
+    :multiple="true"
+    :auto-upload="false"
+    :on-change="onChange"
+    list-type="picture-card"
+    accept=".gif,.jpg,.jpeg,.png,.bmp"
+  >
+    <template #default>
+      <i class="el-icon-plus"></i>
+    </template>
+    <template #file="{ file }">
+      <div>
+        <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+        <span class="el-upload-list__item-actions">
+          <span class="el-upload-list__item-preview" @click="preview(file)">
+            <i class="el-icon-zoom-in"></i>
+          </span>
+          <span class="el-upload-list__item-delete" @click="remove(file)">
+            <i class="el-icon-delete"></i>
+          </span>
+        </span>
+      </div>
+    </template>
+  </el-upload>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+  emits: ['onChange', 'preview'],
+  setup (props, { emit }) {
+    const state = reactive({
+      fileList: [] as Array<any>
+    });
+
+    // 新增文件时
+    const onChange = (file: any, fileList: Array<any>) => {
+      state.fileList = fileList;
+      emit('onChange', state.fileList);
+      console.log(state.fileList);
+    };
+
+    // 移除文件时
+    const remove = (file: any) => {
+      const removeIndex = state.fileList.findIndex((value: any) => value.uid === file.uid);
+      state.fileList.splice(removeIndex, 1);
+      emit('onChange', state.fileList);
+      console.log(state.fileList);
+    };
+
+    // 预览文件(图片或视频)
+    const preview = (file: any) => {
+      emit('preview', file);
+      console.log(file);
+    };
+
+    // 清空文件
+    const clear = () => {
+      state.fileList = [];
+      emit('onChange', state.fileList);
+      console.log('clear');
+    };
+
+    return {
+      onChange,
+      remove,
+      preview,
+      clear
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-upload-list__item {
+  transition: none !important;
+}
+</style>
