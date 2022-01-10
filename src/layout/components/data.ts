0ee4411009e1c4315dc1e146d0099dc8d46c704e
xiecoder <3318461331@qq.com>
2022-01-10 21:16:50 +0800

    refactor: 重构代码

--- src/layout/components/data.ts
+++ src/layout/components/data.ts
@@ -20,7 +20,7 @@ const headerMenuList: Array<any> = [
     title: '园林花卉'
   },
   {
-    index: 'recognition',
+    index: 'ai',
     icon: 'el-icon-search',
     title: 'AI 智能识别'
   },
@@ -31,6 +31,120 @@ const headerMenuList: Array<any> = [
   }
 ];
 
-const headerMenuItemList: any = {};
+const headerMenuItemList: any = {
+  home: {
+    meta: [],
+    path: '/admin/home'
+  },
+  news: {
+    meta: [
+      {
+        name: 'news',
+        icon: 'el-icon-postcard',
+        label: '新闻公告',
+        list: [
+          { name: 'news-articles', label: '新闻管理' },
+          { name: 'news-type', label: '类型管理' }
+        ]
+      }
+    ],
+    path: '/admin/news-articles'
+  },
+  disease: {
+    meta: [
+      {
+        name: 'disease',
+        icon: 'el-icon-first-aid-kit',
+        label: '病害数据库',
+        list: [
+          { name: 'disease-data', label: '植物病害管理' }
+        ]
+      },
+      {
+        name: 'pest',
+        icon: 'el-icon-soccer',
+        label: '虫害数据库',
+        list: [
+          { name: 'pest-data', label: '植物虫害管理' }
+        ]
+      }
+    ],
+    path: '/admin/disease-data'
+  },
+  gardens: {
+    meta: [
+      {
+        name: 'plants',
+        icon: 'el-icon-cherry',
+        label: '植物数据集',
+        list: [
+          { name: 'plants-data', label: '植物管理' },
+          { name: 'plants-family', label: '科属管理' }
+        ]
+      },
+      {
+        name: 'gardens',
+        icon: 'el-icon-video-camera',
+        label: '园林植物护养',
+        list: [
+          { name: 'bonsai-video', label: '盆景养护视频' }
+        ]
+      }
+    ],
+    path: '/admin/plants-data'
+  },
+  ai: {
+    meta: [
+      {
+        name: 'images',
+        icon: 'el-icon-picture',
+        label: 'AI 图像数据集',
+        list: [
+          { name: 'disease-images', label: '病害图像数据集管理' },
+          { name: 'pest-images', label: '虫害图像数据集管理' },
+          { name: 'plant-images', label: '植物图像数据集管理' }
+        ]
+      },
+      {
+        name: 'dataset',
+        icon: 'el-icon-price-tag',
+        label: '数据集标注',
+        list: [
+          { name: 'tagging-images', label: '图片标注管理' }
+        ]
+      },
+      {
+        name: 'model',
+        icon: 'el-icon-menu',
+        label: '机器学习模型训练',
+        list: [
+          { name: 'training-model', label: '训练模型管理' }
+        ]
+      },
+      {
+        name: 'config',
+        icon: 'el-icon-s-tools',
+        label: 'AI 系统配置',
+        list: [
+          { name: 'training-params', label: '训练参数配置' }
+        ]
+      }
+    ],
+    path: '/admin/disease-images'
+  },
+  system: {
+    meta: [
+      {
+        name: 'system',
+        icon: 'el-icon-setting',
+        label: '系统管理',
+        list: [
+          { name: 'role-data', label: '角色管理' }
+        ]
+      }
+    ],
+    path: '/admin/role-data'
+  }
+};
 
 export { headerMenuList, headerMenuItemList };
