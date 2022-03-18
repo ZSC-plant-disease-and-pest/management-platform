09d7c0ff1b5cc61773c6fab954d16b4a682d452f
xiecoder <3318461331@qq.com>
2022-03-18 20:58:31 +0800

    feat: 新增单个视频上传组件

---
+++ src/components/common/BasicVideoUpload/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-upload
+    ref="uploadVideoRef"
+    action="#"
+    :limit="10"
+    :multiple="true"
+    :auto-upload="false"
+    :on-change="onChange"
+    :class="{ 'uploadVideo': isUploadVideo }"
+    list-type="picture-card"
+    accept=".mp4"
+    v-bind="$attrs"
+  >
+    <template #default>
+      <i class="el-icon-plus"></i>
+    </template>
+    <template #file="{ file }">
+      <div>
+        <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+        <span class="el-upload-list__item-actions">
+          <span class="el-upload-list__item-preview" @click="preview(file)">
+            <i class="el-icon-zoom-in"></i>
+          </span>
+          <span class="el-upload-list__item-delete" @click="remove(file)">
+            <i class="el-icon-delete"></i>
+          </span>
+        </span>
+      </div>
+    </template>
+  </el-upload>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+
+export default defineComponent({
+  emits: ['onChange', 'preview'],
+  setup (props, { emit }) {
+    const state = reactive({
+      fileList: [] as Array<any>,
+      isUploadVideo: false
+    });
+
+    // 新增文件时
+    const onChange = (file: any, fileList: Array<any>) => {
+      state.fileList = fileList;
+      state.isUploadVideo = true;
+      emit('onChange', state.fileList);
+    };
+
+    // 移除文件时
+    const remove = (file: any) => {
+      const removeIndex = state.fileList.findIndex((value: any) => value.uid === file.uid);
+      state.fileList.splice(removeIndex, 1);
+      setTimeout(() => {
+        state.isUploadVideo = false;
+      }, 1);
+      emit('onChange', state.fileList);
+    };
+
+    // 预览文件(图片或视频)
+    const preview = (file: any) => {
+      emit('preview', file);
+    };
+
+    // 清空文件
+    const clear = () => {
+      state.fileList.splice(0, state.fileList.length);
+      setTimeout(() => {
+        state.isUploadVideo = false;
+      }, 1);
+      emit('onChange', state.fileList);
+    };
+
+    return {
+      ...toRefs(state),
+      onChange,
+      remove,
+      preview,
+      clear
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-upload-list__item {
+  transition: none !important;
+}
+.uploadVideo ::v-deep .el-upload.el-upload--picture-card {
+  display: none;
+}
+::v-deep .el-upload-list__item.is-ready {
+  margin-bottom: 0px;
+}
+</style>
