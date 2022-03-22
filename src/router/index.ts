a45b3e30ab99ce9d6050c97c0ef31ae7c17db8f6
xiecoder <3318461331@qq.com>
2022-03-22 13:07:14 +0800

    refactor: 重构

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,25 +17,6 @@ const layout = () => import('@/layout/index.vue');
 // 测试
 const test = () => import('@/views/test/index.vue');
 
-// 5 AI 智能识别
-// recognition/diseaseImageManagement 病害图像数据集管理
-const diseaseImageManagement = () => import('@/views/admin/recognition/images/diseaseImageManagement/index.vue');
-// recognition/pestImageManagement 病害图像数据集管理
-const pestImageManagement = () => import('@/views/admin/recognition/images/pestImageManagement/index.vue');
-// recognition/plantsImageManagement 病害图像数据集管理
-const plantsImageManagement = () => import('@/views/admin/recognition/images/plantsImageManagement/index.vue');
-
-// recognition/datasetDetail 病虫害图像数据集详情
-const datasetDetail = () => import('@/views/admin/recognition/images/detail/index.vue');
-
-// recognition/imagesTaggingManagement 图片标注管理
-const imagesTaggingManagement = () => import('@/views/admin/recognition/tagging/taggingManagement/index.vue');
-
-// recognition/trainingModelManagement 训练模型管理
-const trainingModelManagement = () => import('@/views/admin/recognition/model/trainingModelManagement/index.vue');
-// recognition/trainingModelManagement 训练模型详情
-const trainingModelManagementAddUpdate = () => import('@/views/admin/recognition/model/trainingModelManagement/components/add-update.vue');
-
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -56,48 +37,7 @@ const routes: Array<RouteRecordRaw> = [
     name: 'admin',
     redirect: '/admin/home',
     component: layout,
-    children: [
-      {
-        path: 'recognition/diseaseImageManagement',
-        name: 'diseaseImageManagement',
-        component: diseaseImageManagement
-      },
-      {
-        path: 'recognition/pestImageManagement',
-        name: 'pestImageManagement',
-        component: pestImageManagement
-      },
-      {
-        path: 'recognition/plantsImageManagement',
-        name: 'plantsImageManagement',
-        component: plantsImageManagement
-      },
-      {
-        path: 'recognition/datasetDetail/:type/:id',
-        name: 'datasetDetail',
-        component: datasetDetail
-      },
-      {
-        path: 'recognition/imagesTaggingManagement',
-        name: 'imagesTaggingManagement',
-        component: imagesTaggingManagement
-      },
-      {
-        path: 'recognition/trainingModelManagement',
-        name: 'trainingModelManagement',
-        component: trainingModelManagement
-      },
-      {
-        path: 'recognition/trainingModelManagement/add',
-        name: 'trainingModelManagementAdd',
-        component: trainingModelManagementAddUpdate
-      },
-      {
-        path: 'recognition/trainingModelManagement/update',
-        name: 'trainingModelManagementUpdate',
-        component: trainingModelManagementAddUpdate
-      }
-    ]
+    children: []
   }
 ];
 
