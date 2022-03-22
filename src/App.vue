e774163979159f69b60bac1d98050ec2bdd26083
xiecoder <3318461331@qq.com>
2022-03-22 12:07:26 +0800

    fix: 修复文字显示bug

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="zhCn">
+    <router-view />
+  </el-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+
+export default defineComponent({
+  setup () {
+    return {
+      zhCn
+    };
+  }
+});
+</script>
 
 <style lang="scss">
 body {
--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,6 @@ import '@/assets/css/global.scss';
 // 引入 element-plus
 import element from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-// 修改 element-plus 语言为中文
-import locale from 'element-plus/lib/locale/lang/zh-cn';
 // 引入视频播放器组件和样式
 import vue3VideoPlay from 'vue3-video-play';
 import 'vue3-video-play/dist/style.css';
@@ -17,4 +15,4 @@ import 'vue3-video-play/dist/style.css';
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 
-createApp(App).use(store).use(router).use(vue3VideoPlay).use(element, { locale }).mount('#app');
+createApp(App).use(store).use(router).use(vue3VideoPlay).use(element).mount('#app');
