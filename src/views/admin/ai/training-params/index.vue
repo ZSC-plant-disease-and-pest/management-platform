082ed2265fc591a9409d57af20b7041b9b15a76b
xiecoder <3318461331@qq.com>
2022-05-07 15:28:09 +0800

    refactor: 重构代码

--- src/views/admin/ai/training-params/index.vue
+++ src/views/admin/ai/training-params/index.vue
@@ -1,152 +1,149 @@
 <template>
-  <BasicSearch
-    :searchList="searchList"
-    @topButtonClick="topButtonClick"
-  />
-  <BasicTable
-    :tableDataList="tableDataList"
-    :tableColumnList="tableColumnList"
-    :topButtonList="topButtonList"
-    :tableButtonList="tableButtonList"
-    :isLoading="isLoading"
-    @topButtonClick="topButtonClick"
-    @tableButtonClick="tableButtonClick"
-    @sortChange="sortChange"
-  />
-  <BasicPage
-    :pageList="pageList"
-    @handleChange="handleChange"
-  />
+  <el-card class="base-card">
+    <template #header>
+      <span>
+        系统基本信息
+      </span>
+    </template>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <span>CPU型号：{{ serverConfig.cpu }}</span>
+      </el-col>
+      <el-col :span="12">
+        <span>GPU信息：{{ serverConfig.gpu }}</span>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <span>内存大小：{{ serverConfig.memory }}</span>
+      </el-col>
+      <el-col :span="12">
+        <span>硬盘大小：{{ serverConfig.hdd }}</span>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <span>操作系统类型：{{ serverConfig.operatingSystem }}</span>
+      </el-col>
+    </el-row>
+  </el-card>
+  <el-form
+    ref="formRef"
+    class="form-common"
+    size="small"
+    :rules="rules"
+    :model="form"
+    label-width="140px"
+    v-show="status === 'incomplete'"
+  >
+    <el-card class="base-card">
+      <template #header>
+        <span>
+          机器学习训练配置信息
+        </span>
+      </template>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="Python语言版本：">
+            <el-select
+              class="select-common"
+              v-model="form.pythonVersion"
+              placeholder="请选择"
+            >
+              <el-option label="2.7" value="2.7" />
+              <el-option label="3.5" value="3.5" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="深度学习框架：">
+            <el-select
+              class="select-common"
+              v-model="form.frame"
+              placeholder="请选择"
+            >
+              <el-option label="Pytorch" value="Pytorch" />
+              <el-option label="Torch" value="Torch" />
+              <el-option label="Caffe" value="Caffe" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="训练数据目录：">
+            <el-input
+              class="input-common"
+              placeholder="请输入训练数据目录"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item>
+            <el-button :loading="isLoading">
+              返回
+            </el-button>
+            <el-button
+              type="primary"
+              :loading="isLoading"
+            >
+              提交
+            </el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-card>
+  </el-form>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount, onUpdated, reactive, toRefs } from 'vue';
-import { diseaseHttp, diseaseParams } from '@/api/disease';
-import { useRoute, useRouter } from 'vue-router';
-import { ElMessage } from 'element-plus';
-import { searchList, topButtonList, tableButtonList, tableColumnList, pageList } from './data';
-import BasicTable from '@/components/common/BasicTable/index.vue';
-import BasicSearch from '@/components/common/BasicSearch/index.vue';
-import BasicPage from '@/components/common/BasicPage/index.vue';
+import { defineComponent, reactive, toRefs, ref, onBeforeMount } from 'vue';
+import { configHttp, configParams } from '@/api/config';
 
 export default defineComponent({
-  components: { BasicTable, BasicSearch, BasicPage },
   setup () {
-    const route = useRoute();
-    const router = useRouter();
-    onBeforeMount(() => {
-      getDisease();
-    });
-    onUpdated(() => {
-      if (route.params.type === 'refresh') {
-        route.params.type = '';
-        getDisease();
-      }
-    });
-
-    // 数据仓库
     const state = reactive({
-      tableDataList: [] as Array<any>,
-      searchList,
-      topButtonList,
-      tableButtonList,
-      tableColumnList,
-      pageList,
+      form: {
+        pythonVersion: '',
+        frame: ''
+      },
+      serverConfig: {} as configParams,
+      formRef: ref(),
+      rules: {},
       isLoading: false
     });
-
-    // 请求表单数据
-    const diseaseParams = reactive({ page: 0, size: 10 } as diseaseParams);
-    const getDisease = () => {
-      state.isLoading = true;
-      diseaseHttp.getDisease(diseaseParams)
+    const status = ref('incomplete');
+    onBeforeMount(() => {
+      getServerConfig();
+    });
+    const getServerConfig = () => {
+      configHttp.getServerConfig()
         .then((response: any) => {
-          state.pageList.total = response.totalElements;
-          state.pageList.size = response.size;
-          state.tableDataList = [];
-          for (let i = 0; i < response.content.length; i++) {
-            state.tableDataList.push(response.content[i]);
-          }
-        })
-        .finally(() => { state.isLoading = false; });
+          state.serverConfig = response;
+        });
     };
-
-    // 删除
-    const deleteDisease = (selectedIds: any) => {
-      if (selectedIds.length === 0) {
-        ElMessage.warning('请选择需要删除的内容');
-      } else {
-        state.isLoading = true;
-        diseaseHttp.deleteDisease(selectedIds.join(','))
-          .then(() => {
-            ElMessage.success('删除成功');
-            getDisease();
-          })
-          .finally(() => { state.isLoading = false; });
-      }
-    };
-
-    // 头部按键
-    const topButtonClick = (name: string, data: any) => {
-      if (name === 'search') {
-        for (const index in data) {
-          if (data[index].name === 'name') {
-            diseaseParams.name = data[index].value === '' ? undefined : data[index].value;
-          }
-        }
-        getDisease();
-      } else if (name === 'reset') {
-        for (const index in state.searchList) {
-          state.searchList[index].value = '';
-          diseaseParams.name = undefined;
-        }
-        getDisease();
-      } else if (name === 'add') {
-        router.push({ path: route.path + '-page', name: route.name as string + '-page', params: { id: '0' } });
-      } else if (name === 'delete') {
-        deleteDisease(data);
-      }
-    };
-
-    // 表格按键
-    const tableButtonClick = (name: string, data: any) => {
-      if (name === 'view') {
-        window.open(`http://localhost:8082/disease/detail/${data.id}`, '_blank');
-      } else if (name === 'edit') {
-        router.push({ path: route.path + '-page', name: route.name as string + '-page', params: { id: data.id } });
-      }
-    };
-
-    // 排序改变
-    const sortChange = (params: any) => {
-      diseaseParams.sort = params.prop === null ? '' : params.prop + ',' + (params.order === 'descending' ? 'desc' : 'asc');
-      getDisease();
-    };
-
-    // 分页改变
-    const handleChange = (name: string, data: any) => {
-      if (name === 'page') {
-        diseaseParams.page = data - 1;
-        state.pageList.page = data;
-      } else if (name === 'size') {
-        diseaseParams.size = data;
-        diseaseParams.page = 0;
-        state.pageList.size = data;
-        state.pageList.page = 1;
-      }
-      getDisease();
-    };
-
     return {
       ...toRefs(state),
-      topButtonClick,
-      tableButtonClick,
-      sortChange,
-      deleteDisease,
-      handleChange
+      status
     };
   }
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.base-card {
+  width: 100%;
+  margin: 0px auto 25px;
+}
+.el-row {
+  margin: 30px 0px;
+}
+.input-common {
+  width: 80%;
+}
+.select-common {
+  width: 80%;
+}
+</style>
